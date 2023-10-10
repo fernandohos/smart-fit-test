eb6bf7d681d0598faa41c709e33420b17f869326
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-10-10 20:00:36 -0300

    :sparkles: add form component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import ContentHeader from "@components/ContentHeader";
+import Form from "@components/Form";
 import Header from "@components/Header";
 
 export default function Home() {
-  return <div>
-    <Header/>
-  </div>;
+  return (
+    <div>
+      <Header />
+      <main className="m-auto mt-10 w-5/6 sm:max-w-lg md:max-w-2xl lg:max-w-4xl font-gotham">
+        <ContentHeader />
+        <Form />
+      </main>
+    </div>
+  );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import iconHour from "@public/images/icon-hour.png";
+import RadioInput from "./RadioInput";
+import CheckboxInput from "./CheckboxInput";
+import Button from "./Button";
+
+export default function Form() {
+  return (
+    <form className="p-6 border-4 border-light-grey rounded-xl border-opacity-25 my-8">
+      <div className="flex items-center gap-2">
+        <Image src={iconHour} width={40} height={40} alt="ícone de relógio" />
+        <p className="text-light-grey text-lg">Horário</p>
+      </div>
+      <p className="text-2xl font-light text-light-grey p-3 mt-3">Qual período quer treinar?</p>
+      <hr />
+      <RadioInput name="period" value="morning" period="Manhã" hour="06:00 às 12:00" />
+      <RadioInput name="period" value="afternoon" period="Tarde" hour="12:01 às 18:00" />
+      <RadioInput name="period" value="evening" period="Noite" hour="18:01 às 23:00" />
+      <div className="flex flex-col sm:flex-row items-center justify-between mt-14 gap-5">
+        <CheckboxInput name="closed-units" label="Exibir unidades fechadas" />
+        <p>Resultados encontrados: <span className="font-bold text-xl">0</span></p>
+      </div>
+      <div className="flex justify-center items-stretch gap-5 flex-col sm:flex-row mt-7">
+        <Button type="submit">Encontrar Unidade</Button>
+        <Button buttonStyle="outlined" type="reset">Limpar</Button>
+      </div>
+    </form>
+  );
+}
